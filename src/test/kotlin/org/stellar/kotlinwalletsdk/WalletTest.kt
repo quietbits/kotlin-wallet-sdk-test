0e9172fb9bf556c18b4322c2268dbc2760974d10
Iveta <quietbits@gmail.com>
2022-08-10 11:38:27 -0400

    Added Wallet.create() test

--- src/test/kotlin/org/stellar/kotlinwalletsdk/WalletTest.kt
+++ src/test/kotlin/org/stellar/kotlinwalletsdk/WalletTest.kt
@@ -1,4 +1,25 @@
 package org.stellar.kotlinwalletsdk
 
+import org.junit.jupiter.api.Test
+import org.stellar.sdk.Network
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
 class WalletTest {
+    private val wallet = Wallet("https://horizon-testnet.stellar.org", Network.TESTNET.toString())
+
+    @Test
+    fun `create() generates Stellar public and secret keys`() {
+        val accountKeys = wallet.create()
+        val publicKey = accountKeys.publicKey
+        val secretKey = accountKeys.secretKey
+
+//        Public key
+        assertEquals(56, publicKey.length)
+        assertTrue(publicKey.startsWith("G"))
+
+//        Secret key
+        assertEquals(56, secretKey.length)
+        assertTrue(secretKey.startsWith("S"))
+    }
 }
